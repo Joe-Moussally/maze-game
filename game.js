6baa51c9651cd0ac527a0c2b0b0512adaacff569
MJoe111 <joemoussally111@gmail.com>
2022-05-20 00:02:57 +0300

    Adding JS elements

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
+var boundaries = document.querySelectorAll(".boundary");
+var start = document.querySelector("#start");
+var end = document.querySelector("#end");
+var status = document.querySelector("#status");
 
+start.addEventListener("mouseover", function() {
+    document.getElementById("status").innerHTML = "Get to E to win the game";
+
+    //iterate over the bounderies elements and apply mouseover effect
+    for (let i = 0; i < boundaries.length; i++) {
+
+        //When the user hover the walls
+        boundaries[i].addEventListener("mouseover", () => {
+            
+            // adding red background by iterating over all walls
+            for (let j = 0; j < boundaries.length - 1; j++) {
+                boundaries[j].classList.add("youlose");
+            }
+        });
+        }
+        
+});
