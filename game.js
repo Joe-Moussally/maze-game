b93daa35c7ed852bf490e0a68899afd7ea36ac53
MJoe111 <joemoussally111@gmail.com>
2022-05-21 23:30:29 +0300

    Adding basic functions

--- game.js
+++ game.js
@@ -3,60 +3,46 @@ var start = document.querySelector("#start");
 var end = document.querySelector("#end");
 var status = document.getElementById("status");
 var score = 0;
-var active = true;
+var canReset;//reset whole game and clear score
+var canRestart;//can restart the current round of the game
 
-//function for the game
 const maze = () => {
 
-    start.addEventListener("mouseover", () => {
 
-        for (let j=0; j < boundaries.length-1; j++) {
-            boundaries[j].classList.remove("youlose");
-        }
 
-        document.getElementById("status").innerHTML = "Get to the end 'E' to win";
-        
-        //adding boundries mouseover effect
-        for (let i=0; i < boundaries.length-1; i++) {
-            boundaries[i].addEventListener("mouseover", () => {
-                
-                //turning all boundries red
-                for (let j=0; j < boundaries.length-1; j++) {
-                    boundaries[j].classList.add("youlose");
-                }
-
-                document.getElementById("status").innerHTML = "YOU LOST. Move mouse over 'S' to retry, or Click 'S' to reset.";
-
-            })
-        }
-
-        start.addEventListener("click", reset)
-
-    })
 }
 
-
-//reset function
-const reset = () => {
-    //turning all boundries grey
-    for (let j=0; j < boundaries.length-1; j++) {
-        boundaries[j].classList.remove("youlose");
+//function that targets all boudaries and turns them red
+const redWalls = () => {
+    for (let i = 0; i < boundaries.length - 1; i++) {
+        boundaries[i].classList.add("youlose")
     }
-
-    maze()
 }
 
+//function that targets all boudaries and turns them grey
+const greyWalls = () => {
+    for (let i = 0; i < boundaries.length - 1; i++) {
+        boundaries[i].classList.remove("youlose")
+    }
+}
+
+//function that executes when user reaches "E"
 const win = () => {
+    canReset = true;
+    canRestart = true;
+}
 
-    score += 5;
+//function that handles the game and score when the user restarts the round
+const restart = () => {
+    
+}
 
-    alert("You won! --Score: "+score+);
-    maze();
-
+//function that handles the game and score when the user resets the game
+const reset = () => {
 
 }
 
-end.addEventListener("mouseover",win)
+start.addEventListener("mouseover",redWalls)
 
 //executing script on successful load
 window.onload = function () {
