59ee434815bfe982e6e9de2830c470a4ce892ede
MJoe111 <joemoussally111@gmail.com>
2022-05-22 00:53:18 +0300

    Adding cheating function

--- game.js
+++ game.js
@@ -3,12 +3,12 @@ var start = document.querySelector("#start");
 var end = document.querySelector("#end");
 var title = document.getElementById("status");
 var score = 0;//game score
-// var canReset;//reset whole game and clear score
-// var canRestart;//can restart the current round of the game
+const game = document.getElementById("game");//user for cheating
 
 const maze = () => {
 
     greyWalls()
+    game.addEventListener("mouseleave",cheat);
     title.innerHTML = "Get to E to win. --SCORE: " + score;
     //adding event listeners when the game begins
     end.addEventListener("mouseover",win);
@@ -43,6 +43,7 @@ const win = () => {
     for (let i = 0; i < boundaries.length - 1; i++) {
         boundaries[i].removeEventListener("mouseover",lose);
     }
+    game.removeEventListener("mouseleave",cheat);
 
     score += 5;
     title.innerHTML = "YOU WON!(Click S: Reset / Move to S: Restart) --SCORE: " + score;
@@ -60,12 +61,16 @@ const restart = () => {
 //function that handles the game and score when the user resets the game
 const reset = () => {
     alert("GAME HAS BEEN RESET");
+    
+
+    //reseting game properties before 
     score = 0;
     title.innerHTML = "Begin by moving your mouse over the 'S'.";
     for (let i = 0; i < boundaries.length - 1; i++) {
         boundaries[i].removeEventListener("mouseover",lose);
     }
     end.removeEventListener("mouseover",win);
+    game.removeEventListener("mouseleave",cheat);
     start.addEventListener("mouseover",maze);
     
 }
@@ -78,6 +83,7 @@ const lose = () => {
         boundaries[i].removeEventListener("mouseover",lose);
     }
     end.removeEventListener("mouseover",win);
+    game.removeEventListener("mouseleave",cheat);
 
     redWalls()
 
@@ -88,6 +94,21 @@ const lose = () => {
     //adding event listener to start for restart and reset
     start.addEventListener("mouseover",restart);
     start.addEventListener("click",reset);
+}
+
+//function executed when user leaves the game container aka cheating
+const cheat = () => {
+    alert("NO CHEATING!!! (score reset)");
+    //reset score after cheating
+    score = 0;
+    end.removeEventListener("mouseover",win);
+    game.removeEventListener("mouseleave",cheat);
+    start.addEventListener("mouseover",maze)
+    for (let i = 0; i < boundaries.length - 1; i++) {
+        boundaries[i].removeEventListener("mouseover",lose);
+    }
+    title.innerHTML = "Begin by moving your mouse over the 'S'.";
+
 }
 
 //executing game script on successful load
